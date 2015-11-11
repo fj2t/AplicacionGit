376f10b743e75c6ca7425e5de1846ba609f2e136
francisco javier gil blanco <fj2torres@gmail.com>
2015-11-11 13:56:01 +0100

    Initial commit

---
+++ app/src/main/java/com/example/alumno/aplicaciongit/AplicacionGitFragment.java
@@ -0,0 +1,22 @@
+package com.example.alumno.aplicaciongit;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class AplicacionGitFragment extends Fragment {
+
+    public AplicacionGitFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_aplicacion_git, container, false);
+    }
+}
